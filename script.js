6866a05ea735a02ae4cd821a53bbe82e1290e84b
ronitimonen <ronitim@metropolia.fi>
2024-03-27 13:16:39 +0200

    ronin commit

--- script.js
+++
@@ -1,7 +0,0 @@
-function showRegistrationPopup() {
-    document.getElementById('registration-popup').style.display = 'block';
-}
-
-function closeRegistrationPopup() {
-    document.getElementById('registration-popup').style.display = 'none';
-}
