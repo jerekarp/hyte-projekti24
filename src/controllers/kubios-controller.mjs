d4d86e1b0bf29c777070302a26d92943fbab64b5
jerekarp <jerep.karppinen@gmail.com>
2024-03-28 10:27:09 +0200

    Lisätty kubios datan filtteröintiä

--- src/controllers/kubios-controller.mjs
+++ src/controllers/kubios-controller.mjs
@@ -53,4 +53,36 @@ const getUserInfo = async (req, res, next) => {
   return res.json(userInfo);
 };
 
-export {getUserData, getUserInfo};
+const getFilteredData = async (req, res, next) => {
+  try {
+    const { kubiosIdToken } = req.user;
+    const headers = new Headers();
+    headers.append('User-Agent', process.env.KUBIOS_USER_AGENT);
+    headers.append('Authorization', kubiosIdToken);
+
+    const response = await fetch(baseUrl + '/result/self?from=2022-01-01T00%3A00%3A00%2B00%3A00', {
+      method: 'GET',
+      headers: headers,
+    });
+
+    const data = await response.json();
+
+    // Suorita filtteröinti
+    const filteredData = data.results.map(result => ({
+      measured_timestamp: result.measured_timestamp,
+      stress_index: result.result.stress_index,
+      respiratory_rate: result.result.respiratory_rate,
+      mean_hr_bpm: result.result.mean_hr_bpm,
+      rmssd_ms: result.result.rmssd_ms,
+      user_happiness: result.user_happiness
+    }));
+
+    return res.json({ status: 'ok', filteredData });
+  } catch (error) {
+    // Käsittely virheitä, jos niitä tapahtuu
+    console.error('Error occurred:', error);
+    return res.status(500).json({ status: 'error', message: 'Internal server error' });
+  }
+};
+
+export {getUserData, getUserInfo, getFilteredData};
