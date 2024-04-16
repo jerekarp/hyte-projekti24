c120e27cf26bcfa9ae9e462bae4a8ff8df590b32
ronitimonen <ronitim@metropolia.fi>
2024-04-16 17:38:27 +0300

    chat ikkuna

--- public/about.js
+++ public/about.js
@@ -1,4 +1,25 @@
-import { fetchData } from './fetch.js';
+document.addEventListener('DOMContentLoaded', function() {
+  // 1 Modalin viittaus
+  var modal = document.getElementById("myModal");
+  var btn = document.getElementById("avaaNappi");
+  var span = document.getElementsByClassName("close")[0];
+  // modal on piilossa kunnes sitä kutsutaan.
+  modal.style.display = "none";
+  // Kun käyttäjä klikkaa nappia, avaa modal
+  btn.addEventListener('click', function() {
+    modal.style.display = "block";
+  });
+  // Kun käyttäjä klikkaa <span> (x), sulje modal
+  span.addEventListener('click', function() {
+    modal.style.display = "none";
+  });
+  // Kun käyttäjä klikkaa modalin ulkopuolella, sulje se
+  window.addEventListener('click', function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  });
+});
 
 document.querySelector(".nav-link.nav-link-right").addEventListener("click", logOut);
 
