d0777cbcd157ae2ee955669163e37345e6c8fe9a
ronitimonen <ronitim@metropolia.fi>
2024-04-23 15:49:54 +0300

    tyylittelyä

--- public/about.js
+++ public/about.js
@@ -34,8 +34,34 @@ document.addEventListener('DOMContentLoaded', function() {
 
 });
 
+// ZenBotin infonappi
+
+// Modaali Info
+let modal = document.getElementById("modal");
+let icon = document.getElementById("info-icon");
+let close = document.getElementsByClassName("close1")[0];
+
+modal.style.display = "none";
+
+// Näytetään modaalinen ikkuna kun klikkaat kuvaketta
+icon.addEventListener("click", function() {
+  modal.style.display = "block";
+});
+
+// Piilotetaan modaalinen ikkuna kun klikkaat sulkemispainiketta
+close.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 // Frontend koodi (esimerkki käyttäen Fetch API:a)
 const sendMessage = async (message) => {
+  document.getElementById('loader').style.display = 'block';
   try {
     const response = await fetch('http://localhost:3000/api/chat/zenbot', {
       method: 'POST',
@@ -57,9 +83,21 @@ const sendMessage = async (message) => {
     document.getElementById('chat-output').innerText = 'Error: ' + error.message; // Näyttää virheviestin sivulla
   }
   finally {
+    document.getElementById('loader').style.display = 'none';
     document.getElementById('chat-input').value = ''; // Tyhjentää chattikentän
   }
 }
+
+// loadingscreen animaatio
+document.getElementById("chat-input").addEventListener("keydown", function(event) {
+  if (KeyboardEvent.keyCode === 13) { // Enter-näppäin on keycode 13
+    event.preventDefault(); // Estää oletustoiminnon, kuten lomakkeen lähetyksen
+    const message = event.target.value; // Haetaan viesti input-kentästä
+    if (message) {
+      sendMessage(message);
+    }
+  }
+});
 
 // Kirjautuminen ulos navbarin kirjaudu ulos painikkeesta
 document.querySelector(".nav-link.nav-link-right").addEventListener("click", logOut);
